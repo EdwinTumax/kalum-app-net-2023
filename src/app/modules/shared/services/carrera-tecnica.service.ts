fb4546ddd3b77e7129c92e15e0a52f3677def9c4
Edwin Tumax <edwintumax@kinal.edu.gt>
2023-11-11 11:11:42 -0600

    Finalizando el mantenimiento de carreras técnicas

---
+++ src/app/modules/shared/services/carrera-tecnica.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/app/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+const BASE_URL = `${environment.BASE_URL_KALUM_MANAGEMENT}/v1`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarreraTecnicaService {
+
+  constructor(private http: HttpClient) { }
+
+  getCarreras(){
+    return this.http.get(`${BASE_URL}/carreras-tecnicas`);
+  }
+
+  addCarreraTecnica(body: any){
+    return this.http.post(`${BASE_URL}/carreras-tecnicas`,body);
+  }
+
+  updateCarreraTecnica(body: any){
+    return this.http.put(`${BASE_URL}/carreras-tecnicas/${body.carreraId}`,body);
+  }
+
+  deleteCarreraTecnica(carreraId: any){
+    return this.http.delete(`${BASE_URL}/carreras-tecnicas/${carreraId}`);
+  }
+  
+}
